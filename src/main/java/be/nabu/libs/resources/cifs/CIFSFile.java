7e44f3fb637e0b0ecd75b38fde08cf0472ce6e02
alex <alex@nabu.be>
2015-11-05 10:08:27 +0100

    Added last modified support

--- src/main/java/be/nabu/libs/resources/cifs/CIFSFile.java
+++ src/main/java/be/nabu/libs/resources/cifs/CIFSFile.java
@@ -2,19 +2,22 @@ package be.nabu.libs.resources.cifs;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
 
+import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
 import jcifs.smb.SmbFileOutputStream;
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.ResourceContainer;
+import be.nabu.libs.resources.api.TimestampedResource;
 import be.nabu.libs.resources.api.WritableResource;
 import be.nabu.utils.io.IOUtils;
 import be.nabu.utils.io.api.ByteBuffer;
 import be.nabu.utils.io.api.ReadableContainer;
 import be.nabu.utils.io.api.WritableContainer;
 
-public class CIFSFile extends CIFSResource implements ReadableResource, WritableResource {
+public class CIFSFile extends CIFSResource implements ReadableResource, WritableResource, TimestampedResource {
 
 	protected CIFSFile(URI uri, ResourceContainer<?> parent, SmbFile file) {
 		super(uri, parent, file);
@@ -28,6 +31,16 @@ public class CIFSFile extends CIFSResource implements ReadableResource, Writable
 	@Override
 	public ReadableContainer<ByteBuffer> getReadable() throws IOException {
 		return IOUtils.wrap(new SmbFileInputStream(getFile()));
+	}
+
+	@Override
+	public Date getLastModified() {
+		try {
+			return new Date(getFile().lastModified());
+		}
+		catch (SmbException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }
